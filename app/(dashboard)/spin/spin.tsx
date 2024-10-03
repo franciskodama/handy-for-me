b39e725d10007f07cc25b5e1bf67a0a2495cdba3
Francis Kodama <fk@fkodama.com>
2024-10-02 20:41:02 -0400

    Function get item is ready but need a fix on spin page yet

--- app/(dashboard)/spin/spin.tsx
+++ app/(dashboard)/spin/spin.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { LoaderPinwheelIcon } from 'lucide-react';
-import { useState } from 'react';
+import { use, useState } from 'react';
 
 import {
   Card,
@@ -10,8 +10,15 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card';
-import { SpinList } from '@/lib/types';
-import { addSpinList } from '@/lib/actions';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
+import { SpinList, SpinListItem } from '@/lib/types';
+import { addSpinItem, addSpinList, getSpinItemsFromList } from '@/lib/actions';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
@@ -22,67 +29,120 @@ export default function Spin({
   uid: string;
   lists: SpinList[];
 }) {
-  const [list, setList] = useState<string>('');
-  const [pending, setPending] = useState<boolean>(false);
+  const [newList, setNewList] = useState<string>('');
+  const [selectedListName, setSelectedListName] = useState<string>('');
+  const [itemList, setItemList] = useState<string>('');
+  const [pendingNewList, setPendingNewList] = useState<boolean>(false);
+  const [pendingNewItem, setPendingNewItem] = useState<boolean>(false);
+
+  const selectedList = lists.find((list) => list.name === selectedListName);
+  const listId = selectedList?.id;
 
   const handleCreateNewList = async () => {
-    setPending(true);
-    const success = await addSpinList(uid, list);
+    setPendingNewList(true);
+    const success = await addSpinList(uid, newList);
     if (success) {
-      setPending(false);
+      setPendingNewList(false);
+      setNewList('');
     }
   };
+
+  const handleCreateNewItemList = async () => {
+    setPendingNewItem(true);
+    const success = listId && (await addSpinItem(uid, listId, itemList));
+    if (success) {
+      setPendingNewItem(false);
+      setItemList('');
+    }
+  };
+
+  const itemsObjects = listId && use(getSpinItemsFromList(uid, listId) || []);
+  console.log('---  🚀 ---> | itemsObjects:', itemsObjects);
+  // const itemsOfSelectedList: string[] = itemsObjects?.data?.reduce(
+  //   (acc: any[], item: any) => {
+  //     if (!acc.includes(item.name) {
+  //       acc.push(item.name);
+  //     }
+  //     return acc;
+  //   }
+  // );
+
+  // console.log('---  🚀 ---> | itemsOfSelectedList:', itemsOfSelectedList);
 
   return (
     <Card>
       <CardHeader className="mb-4">
-        <CardTitle className="flex items-center gap-2">
-          Spin Magic
-          <LoaderPinwheelIcon className="h-6 w-6" />
-        </CardTitle>
+        <CardTitle className="flex items-center gap-2">Spin Magic</CardTitle>
         <CardDescription>
           A fun, random decision-maker that spins the wheel to pick your next
           adventure!
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="flex flex-col items-center gap-8 mb-4 w-full">
-          <div className="flex w-full">
-            <div className="flex w-1/2 items-center gap-2">
-              <p className="text-sm font-semibold w-[18ch]">Choose a List:</p>
-              <Input placeholder="xxxxx" />
+        <div className="flex items-center justify-between gap-8 mb-4 w-full">
+          <div className="flex w-1/2 flex-col">
+            <div className="flex flex-col w-[25em] gap-2">
+              <Select onValueChange={(value) => setSelectedListName(value)}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Choose a List" />
+                </SelectTrigger>
+                <SelectContent>
+                  {lists.map((item: SpinList) => (
+                    <div key={item.id}>
+                      {item.name && (
+                        <SelectItem value={item.name}>{item.name}</SelectItem>
+                      )}
+                    </div>
+                  ))}
+                </SelectContent>
+              </Select>
+              <div className="flex w-full gap-2">
+                <Input
+                  className="w-full"
+                  placeholder="Enter a new Item for this list"
+                  value={itemList}
+                  onChange={(e) => setItemList(e.target.value)}
+                />
+                <Button
+                  className="w-[10ch]"
+                  onClick={handleCreateNewItemList}
+                  disabled={pendingNewItem || itemList.trim() === ''}
+                >
+                  {pendingNewList ? 'Adding...' : 'Add'}
+                </Button>
+              </div>
             </div>
+          </div>
 
-            <div className="flex w-1/2 items-center gap-2">
+          <div className="flex flex-col w-[25em]">
+            <p className="text-sm h-10 py-2">
+              Do you want to start a new list?
+            </p>
+            <div className="flex items-center gap-2">
               <Input
                 placeholder="List's Name"
-                value={list}
-                onChange={(e) => setList(e.target.value)}
+                value={newList}
+                onChange={(e) => setNewList(e.target.value)}
               />
-              {/* <p>{list}</p> */}
               <Button
-                className={pending ? 'bg-orange-500' : ''}
+                className={pendingNewList ? 'bg-orange-500' : ''}
                 onClick={handleCreateNewList}
-                disabled={pending || list.trim() === ''}
+                disabled={pendingNewList || newList.trim() === ''}
               >
-                {pending ? 'Creating...' : 'Create a New List'}
+                {pendingNewList ? 'Creating...' : 'Create a New List'}
               </Button>
             </div>
           </div>
-          <div className="flex w-full">
-            <div className="flex items-center w-1/2">
-              <p className="text-sm font-semibold w-[18ch]">Chosen List:</p>
-              {lists.map((item: SpinList) => (
-                <p key={item.id} className="text-sm font-semibold w-[18ch]">
-                  {item.name}
-                </p>
-              ))}
-            </div>
-            <div className="flex w-1/2 items-center gap-2">
-              <Input placeholder="Enter an Item" />
-              <Button>Add</Button>
-            </div>
-          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-sm font-semibold">Items:</p>
+          {/* {itemsOfSelectedList &&
+            itemsOfSelectedList.map((item: string) => (
+              <p key={item} className="text-sm w-[18ch]">
+                {item}
+              </p>
+            ))} */}
         </div>
       </CardContent>
     </Card>
--- lib/actions.tsx
+++ lib/actions.tsx
@@ -3,35 +3,6 @@
 import { v4 } from 'uuid';
 import { prisma } from './prisma';
 
-export async function addSpinItem(uid: string, listId: string, item: string) {
-  try {
-    await prisma.spinItem.create({
-      data: {
-        id: v4(),
-        createdAt: new Date(),
-        uid,
-        listId,
-        item,
-        selected: true
-      }
-    });
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
-}
-
-// model SpinItem {
-//   id        String   @id @default(uuid()) @db.VarChar(255)
-//   createdAt DateTime @db.Date
-//   uid       String   @db.VarChar(255)
-//   list      SpinList @relation(fields: [listId], references: [id])
-//   item      String   @db.VarChar(255)
-//   selected Boolean   @default(true)
-//   listId   String
-// }
-
 export async function addSpinList(uid: string, name: string) {
   try {
     await prisma.spinList.create({
@@ -63,12 +34,66 @@ export async function getSpinLists(uid: string) {
   }
 }
 
-export async function getUsers() {
+export async function addSpinItem(uid: string, listId: string, name: string) {
   try {
-    const data = await prisma.user.findMany();
+    await prisma.spinItem.create({
+      data: {
+        id: v4(),
+        createdAt: new Date(),
+        uid,
+        listId,
+        name,
+        selected: true
+      }
+    });
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+export async function getSpinItemsFromList(uid: string, listId: string) {
+  try {
+    const data = await prisma.spinItem.findMany({
+      where: {
+        uid,
+        AND: {
+          listId
+        }
+      }
+    });
     return data;
   } catch (error) {
     console.log(error);
-    return error;
+    return [];
   }
 }
+
+// export type SpinListItem = {
+//   id: string;
+//   createdAt: Date;
+//   uid: string;
+//   listId: string;
+//   name: string;
+//   selected: boolean;
+// };
+
+// model SpinItem {
+//   id        String   @id @default(uuid()) @db.VarChar(255)
+//   createdAt DateTime @db.Date
+//   uid       String
+//   list      SpinList @relation(fields: [listId], references: [id])
+//   item      String
+//   selected Boolean   @default(true)
+//   listId   String
+// }
+// export async function getUsers() {
+//   try {
+//     const data = await prisma.user.findMany();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//     return error;
+//   }
+// }
