092dd51746e334dbeceeb1b0d5b4d164ac2edfbe
Francis Kodama <fk@fkodama.com>
2024-10-03 14:36:44 -0400

    Message to explain the features

--- app/(dashboard)/spin/spin.tsx
+++ app/(dashboard)/spin/spin.tsx
@@ -1,6 +1,14 @@
 'use client';
 
 import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import {
+  BellRing,
+  CircleHelp,
+  LoaderPinwheel,
+  MessageCircleQuestion,
+  SplineIcon
+} from 'lucide-react';
 
 import {
   Card,
@@ -16,11 +24,17 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select';
-import { SpinList, SpinItem } from '@/lib/types';
-import { addSpinItem, addSpinList } from '@/lib/actions';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { LoaderPinwheel, SplineIcon } from 'lucide-react';
+import { SpinList, SpinItem } from '@/lib/types';
+import { addSpinItem, addSpinList } from '@/lib/actions';
+import Explanation from '@/components/Explanation';
 
 export default function Spin({
   uid,
@@ -40,6 +54,7 @@ export default function Spin({
   const [pendingNewItem, setPendingNewItem] = useState<boolean>(false);
   const [spinning, setSpinning] = useState<boolean>(false);
   const [result, setResult] = useState<string>('');
+  const [openAction, setOpenAction] = useState(false);
 
   const handleCreateList = async () => {
     setPendingNewList(true);
@@ -82,16 +97,65 @@ export default function Spin({
   return (
     <Card>
       <CardHeader className="mb-4">
-        <CardTitle className="flex items-center gap-2">Spin Magic</CardTitle>
+        <CardTitle className="flex justify-between items-center gap-2">
+          <h1>Spin Magic</h1>
+
+          {!openAction ? (
+            <>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <motion.button
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{ scale: 0.9 }}
+                    >
+                      <Button
+                        className="text-sm"
+                        variant="link"
+                        onClick={() => {
+                          setOpenAction(true);
+                        }}
+                      >
+                        <CircleHelp className="h-6 w-6" strokeWidth={1.6} />
+                      </Button>
+                    </motion.button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p className="text-primary ml-2 lowercase font-light">
+                      learn more
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </>
+          ) : (
+            <div />
+          )}
+        </CardTitle>
         <CardDescription>
           A fun, random decision-maker that spins the wheel to pick your next
           adventure!
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="flex items-center justify-between gap-8 mb-4 w-full">
-          <div className="flex w-1/2 flex-col">
-            <div className="flex flex-col w-[25em] gap-2">
+        <AnimatePresence>
+          {openAction ? (
+            <motion.div
+              layout
+              initial={{ opacity: 0, y: 50, scale: 0.3 }}
+              animate={{ opacity: 1, y: 0, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.5, transition: { duration: 0.2 } }}
+            >
+              <div className="mb-12">
+                <Explanation setOpenAction={setOpenAction} />
+              </div>
+            </motion.div>
+          ) : null}
+        </AnimatePresence>
+        {/* ----------------------- First Column ----------------------- */}
+        <div className="flex justify-between gap-8 mb-4 w-full">
+          <div className="flex w-1/3 flex-col">
+            <div className="flex flex-col gap-2">
               <Select onValueChange={(value) => setListId(value)}>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Choose a List" />
@@ -122,57 +186,70 @@ export default function Spin({
                 </Button>
               </div>
             </div>
+
+            {/* ----------------------- Second Column ----------------------- */}
+
+            {itemsStrings.length > 0 && (
+              <div className="flex flex-col gap-2 w-[25em]">
+                <p className="text-sm font-semibold mt-4">Items:</p>
+                {itemsStrings.map((item: string) => (
+                  <p key={item} className="text-sm w-full border p-2">
+                    {item}
+                  </p>
+                ))}
+              </div>
+            )}
           </div>
 
-          <div className="flex flex-col w-[25em]">
-            <p className="text-sm h-10 py-2">
-              Do you want to start a new list?
-            </p>
-            <div className="flex items-center gap-2">
-              <Input
-                placeholder="List's Name"
-                value={listInput}
-                onChange={(e) => setListInput(e.target.value)}
-              />
+          {/* ----------------------- Third Column ----------------------- */}
+
+          <div className="flex flex-col w-1/3 border h-[10em]">
+            {itemsStrings.length > 0 && (
               <Button
-                className={pendingNewList ? 'bg-orange-500' : ''}
-                onClick={handleCreateList}
-                disabled={pendingNewList || listInput.trim() === ''}
+                onClick={handleSpin}
+                // disabled={Boolean(listId)}
               >
-                {pendingNewList ? 'Creating...' : 'Create a New List'}
+                {spinning ? 'Spinning...' : 'Spin Magic'}
+                <LoaderPinwheel
+                  className={`w-4 h-4 ml-2 ${spinning ? 'animate-spin' : null}`}
+                />
               </Button>
+            )}
+
+            {result && (
+              <>
+                <p className="text-5xl text-white p-4 text-center border bg-orange-500 animate-pulse w-full mt-8">
+                  {result}
+                </p>
+                <Button variant="ghost" onClick={() => setResult('')}>
+                  clear
+                </Button>
+              </>
+            )}
+          </div>
+
+          <div className="flex flex-col w-1/3">
+            <div className="w-[25em]">
+              <p className="text-sm h-10 py-2">
+                Do you want to start a new list?
+              </p>
+              <div className="flex items-center gap-2">
+                <Input
+                  placeholder="List's Name"
+                  value={listInput}
+                  onChange={(e) => setListInput(e.target.value)}
+                />
+                <Button
+                  className={pendingNewList ? 'bg-orange-500' : ''}
+                  onClick={handleCreateList}
+                  disabled={pendingNewList || listInput.trim() === ''}
+                >
+                  {pendingNewList ? 'Creating...' : 'Create a New List'}
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="flex flex-col gap-2 w-[25em]">
-          <p className="text-sm font-semibold mt-4">Items:</p>
-          {itemsStrings &&
-            itemsStrings.map((item: string) => (
-              <p key={item} className="text-sm w-full border p-2">
-                {item}
-              </p>
-            ))}
-          <Button
-            onClick={handleSpin}
-            // disabled={Boolean(listId)}
-          >
-            {spinning ? 'Spinning...' : 'Spin Magic'}
-            <LoaderPinwheel
-              className={`w-4 h-4 ml-2 ${spinning ? 'animate-spin' : null}`}
-            />
-          </Button>
-        </div>
-        {result && (
-          <>
-            <p className="text-5xl text-white p-4 text-center border bg-orange-500 animate-pulse w-full mt-8">
-              {result}
-            </p>
-            <Button variant="ghost" onClick={() => setResult('')}>
-              clear
-            </Button>
-          </>
-        )}
       </CardContent>
     </Card>
   );
---
+++ components/Explanation.tsx
@@ -0,0 +1,111 @@
+'use client';
+
+import { Bomb, MessageCircle, X } from 'lucide-react';
+import { Alert, AlertDescription } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+
+export default function Explanation({
+  setOpenAction
+}: {
+  setOpenAction: (value: boolean) => void;
+}) {
+  const handleClickMessageButton = () => {
+    const email = process.env.NEXT_PUBLIC_MY_UID;
+
+    if (!email) {
+      console.error('Email environment variable not set');
+      return;
+    }
+
+    const subject = encodeURIComponent(
+      '[HANDYFOR.ME] I have something to tell you!'
+    );
+    const body = encodeURIComponent(`
+      Hey Handy For Me Team, 👋
+      
+      I would like to share something about your app:
+      
+      
+
+
+      Cheers! 🍻
+      `);
+
+    window.open(
+      `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`,
+      '_blank'
+    );
+  };
+
+  return (
+    <div className="relative">
+      <Alert
+        style={{
+          borderImage: `repeating-linear-gradient(
+              45deg,
+              transparent,
+              transparent 2.5px,
+              black 3px,
+              black 3px,
+              transparent 3px,
+              transparent 3px
+            ) 15 / 0.75rem`,
+          borderStyle: 'solid',
+          borderWidth: '1em'
+        }}
+      >
+        <AlertDescription className="relative flex flex-wrap items-start justify-between p-2">
+          <div className="flex flex-col">
+            <div className="flex items-center mb-2">
+              <Bomb size={24} color="black" strokeWidth={1.8} />
+              <p className="font-bold text-lg ml-2">Ops...</p>
+            </div>
+            <h3 className="text-primary text-xs w-[26ch]">
+              Looks like we’re missing
+              <br />
+            </h3>
+          </div>
+
+          <div className="flex flex-wrap justify-between">
+            <div>
+              <p className="font-bold text-lg mb-2">Why?</p>
+              <p className="text-xs w-[30ch]">
+                Well, Francis is still pinching pennies and hasn’t paid for that
+                fancy API to fetch the stock prices automatically!
+              </p>
+            </div>
+            <div>
+              <p className="font-bold text-lg mb-2 text-transparent">X</p>
+              <p className="text-xs w-[30ch] ml-2">
+                But don’t worry, once he manually updates the price in his
+                trusty spreadsheet, you’ll see it here.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col mr-20">
+            <p className="text-primary text-xs w-[35ch] mb-4">
+              As his friend, give him a quick nudge to add
+              <br />
+              {`Help Francis Out. :)`}
+            </p>
+            <Button
+              variant={'outline'}
+              className="flex items-center border-2 border-primary capitalize"
+              onClick={handleClickMessageButton}
+            >
+              Send a message to our Team!
+              <MessageCircle className="ml-2" size={24} strokeWidth={1.8} />
+            </Button>
+          </div>
+          <button
+            className="absolute right-2 top-2"
+            onClick={() => setOpenAction(false)}
+          >
+            <X size={24} color="black" strokeWidth={1.8} />
+          </button>
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
+}
