8160b48674d6ced82a0289363ecb7deeabeaad12
Francis Kodama <fk@fkodama.com>
2024-10-20 15:24:02 -0400

    Prisma, types, and actions

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,5 @@
+import { shortcut_color_enum } from '@prisma/client';
+
 export type User = {
   name?: string | null;
   email?: string | null;
@@ -37,4 +39,24 @@ export type BucketListItem = {
   name: string;
   category: string;
   done: boolean;
+};
+
+export type ShortcutCategory = {
+  id: string;
+  createdAt: Date;
+  uid: string;
+  name: string;
+  color: shortcut_color_enum;
+  shortcuts: Shortcut[] | null;
+};
+
+export type Shortcut = {
+  id: string;
+  createdAt: Date;
+  uid: string;
+  name: string;
+  url: string;
+  description: string;
+  categoryId: string;
+  category: string;
 };
