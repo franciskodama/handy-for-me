ade37f18d6c027318a111e92349d10a7102377e2
Francis Kodama <fk@fkodama.com>
2026-01-29 20:12:43 -0500

    Prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,9 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
-import ws from 'ws';
+import { PrismaNeon } from '@prisma/adapter-neon';
 
-neonConfig.webSocketConstructor = ws;
+const connectionString = process.env.DATABASE_URL as string;
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
+const adapter = new PrismaNeon({ connectionString });
 
 declare global {
   var prisma: PrismaClient | undefined;
@@ -21,3 +16,27 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 export default prisma;
+
+// import { Pool, neonConfig } from '@neondatabase/serverless';
+// import { PrismaNeon } from '@prisma/adapter-neon';
+// import { PrismaClient } from '@prisma/client';
+// import ws from 'ws';
+
+// neonConfig.webSocketConstructor = ws;
+
+// const connectionString = `${process.env.DATABASE_URL}`;
+
+// const pool = new Pool({ connectionString });
+// const adapter = new PrismaNeon(pool);
+
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// const prisma = global.prisma || new PrismaClient({ adapter });
+
+// if (process.env.NODE_ENV !== 'production') {
+//   global.prisma = prisma;
+// }
+
+// export default prisma;
