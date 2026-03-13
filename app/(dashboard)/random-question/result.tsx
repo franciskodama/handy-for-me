63d6d8c36ec6f851d9b926ff47569b31de48687a
Francis Kodama <fk@fkodama.com>
2026-03-13 16:10:48 -0400

    Result

--- app/(dashboard)/random-question/result.tsx
+++ app/(dashboard)/random-question/result.tsx
@@ -8,10 +8,14 @@ export default function Result({
   result: string;
   handleResetAll: () => void;
 }) {
+  // const resultFontSize = result.length > 100 ? 'text-2xl' : 'text-5xl';
+
   return (
     <div className="relative stripe-border flex flex-col w-full items-center">
       {result ? (
         <>
+          <Button>+</Button>
+          <Button>-</Button>
           <p className="flex flex-col justify-center p-12 text-2xl sm:text-5xl font-semibold text-primary sm:leading-normal text-center">
             {result}
           </p>
@@ -27,7 +31,8 @@ export default function Result({
         <>
           <div className="flex flex-col h-full justify-center items-center px-12 py-24 leading-tight text-center w-full gap-4">
             <div className="flex items-center gap-2">
-              <p className="font-semibold text-lg sm:text-2xl">
+              {/* <p className="font-semibold text-6xl sm:text-2xl"> */}
+              <p className="font-semibold text-6xl sm:text-2xl">
                 Ready for a challenge?
               </p>
               <Flame size={32} strokeWidth={1.6} />
